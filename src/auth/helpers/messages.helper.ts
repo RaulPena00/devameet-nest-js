537a0335db58fe6919b71f1416852e48abbe5c31
Raul Pena Pereira <raulppereira00@gmail.com>
2023-05-10 09:29:31 -0300

    Endpoint de Login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOUND: 'Favor preencer o login',
+    AUTH_PASSWORD_NOT_FOUND: 'Favor preencer a senha',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'E-mail e/ou senha não encontrados.',
+}
